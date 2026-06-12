51c687debb7a71cfc8ab57cd905b28d8cb48faa0
maimai12ahv2001 <maimai12ahv2001@gmail.com>
2026-06-12 13:58:07 +0700

    Sua dung duong dan base

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/inventory-management-dashboard/',
+  base: '/quanlythuoc-vattu/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
